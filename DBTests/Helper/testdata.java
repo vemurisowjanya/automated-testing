f7cee80062b6c2f921e5cb3cc626fb2b29fcef4e
Shwetambara <shwetambarasri@gmail.com>
2014-08-18 00:35:47 +0530

    added changes to the common code

--- DBTests/Helper/testdata.java
+++ DBTests/Helper/testdata.java
@@ -1,7 +1,12 @@
 package Helper;
 
+import java.io.File;
+import java.io.IOException;
 import java.sql.Connection;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
 public abstract class testdata {
@@ -15,5 +20,13 @@ public abstract class testdata {
     protected String password = "check*123#";
     protected String driver_class = "com.mysql.jdbc.Driver";
     public Connection connection = null;
+    protected String fileaddr = "/home/shwetambara/ScreenshotOnFail/sample.png";
+
+    public void takeScreenshotonFailure() throws IOException{
+		// take the screenshot at the end of every test
+        File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+        // now save the screenshot to a file some place
+        FileUtils.copyFile(scrFile, new File(fileaddr));   
+	}
 
 }
