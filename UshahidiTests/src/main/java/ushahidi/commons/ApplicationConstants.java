5a279f8adee6aa91651fc2833e40634eb544c319
KeshaShah <kesha.shah1106@gmail.com>
2015-06-13 23:42:23 +0530

    Added Constants file that will be used in application

---
+++ UshahidiTests/src/main/java/ushahidi/commons/ApplicationConstants.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package ushahidi.commons;
+
+/**
+ * @author Kesha
+ *
+ */
+public class ApplicationConstants {
+
+//	public static final String baseUrl = "http://localhost/Ushahidi_Web-master";
+	public static final String baseUrl = "http://pc-crowdmap-dev.systers.org";
+	public static final String extlogin = "/login";
+}
