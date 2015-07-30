a27d09f1d250aad056ee437af4e5646013b59724
Daisy Nkweteyim <nkweteyimdaisy@gmail.com>
2015-07-30 10:03:58 +0100

    added constants for sheet1 and sheet2

--- src/utilities/Constants.java
+++ src/utilities/Constants.java
@@ -4,10 +4,12 @@ public class Constants {
 	
 	public static final String baseURL = "http://localhost:8001";
 	
-	public static final String pathLoginTestData = "src/data/loginDetails.xlsx";
+	public static final String pathTestData = "src/data/loginDetails.xlsx";
 	
 	public static final String loginDataSheetName = "Sheet1";
 	
-	public static final String postDataSheetName = "Sheet2";
+	public static final String accountDataSheetName = "Sheet2";
+	
+	public static final String postDataSheetName = "Sheet3";
 
 }
