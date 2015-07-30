fe10c7e4ac57099d44483b45c7d9a28a0f8d3cd8
Daisy Nkweteyim <nkweteyimdaisy@gmail.com>
2015-07-30 10:05:49 +0100

    common code for input of data from excel sheet

--- src/utilities/ExcelReader.java
+++ src/utilities/ExcelReader.java
@@ -1,62 +1,63 @@
-/**
- * The login test
- *
- * Make sure test works anywhere where login is required
- * 
- * @author daisy
- */
-
 package utilities;
-
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.apache.poi.xssf.usermodel.XSSFCell;
 
 import java.io.FileInputStream;
 
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-public class ExcelLoginReader {
-	private static XSSFSheet ExcelSheet;
+public class ExcelReader {
 	private static XSSFWorkbook ExcelBook;
+	private static XSSFSheet ExcelSheet;
 	private static XSSFCell Cell;
-	
-	//connects to Excel Worksheet and reads the data in each cell
-	public static String[][] read(String path, String sheetName ) throws Exception {	
-	
+
+	//connects to Excel Worksheet 
+	public static Object[][] connect(String path, String sheetName ) throws Exception {	
+		
 		FileInputStream inputStream = new FileInputStream(path);
 
 		ExcelBook = new XSSFWorkbook(inputStream);
 	
 		ExcelSheet = ExcelBook.getSheet(sheetName);
 		
+		//number of times the test will be invoked
+		int totalRows = ExcelSheet.getLastRowNum() ;//0-based(0, 1, 2...)
+				
+		//assumes that each row has same number of columns.
+		int totalCols = ExcelSheet.getRow(0).getLastCellNum();//1-based(1, 2, 3...)
+				
+			
+		Object[][] cellValue = new String [totalRows][totalCols];
+		return cellValue;
+	}
+	
+	//reads the data in each cell
+	public static Object[][] read(Object[][] array){
+		
 		int startRow = 1;
 		
 		int startCol = 0;
-		int totalRows = ExcelSheet.getLastRowNum() ;//0-based(0, 1, 2...)
-		
-		//assumes that each row has same number of columns.
-		int totalCols = ExcelSheet.getRow(0).getLastCellNum();//1-based(1, 2, 3...)
-		
-		String[][] cellValue = new String [totalRows][totalCols];
 		
 		int ci = 0;
-		for (int i = startRow; i <= totalRows ; i++) {
+		for (int i = startRow; i <= array.length ; i++) {
 			int cj = 0;
-			for (int j = startCol; j < totalCols; j++) {
+			for (int j = startCol; j < array[0].length; j++) {
 				
-				cellValue[ci][cj] = getData(i, j);
+				array[ci][cj] = getData(i, j);
 				cj++;
 			}
 			ci++;
 		}	
-		return(cellValue);
+		return (array);
 	}
 	
 	//gets the data in each cell
-	private static String getData(int i, int j) {
+	private static Object getData(int i, int j) {
 		Cell = ExcelSheet.getRow(i).getCell(j);
 		if(Cell.getCellType() == 1)
-			return Cell.getStringCellValue();
-		else return "";
+		return Cell.getStringCellValue();
+		else if(Cell.getCellType() == 4)
+			return Cell.getBooleanCellValue();
+		return "";
 	}
 }
