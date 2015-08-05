887fbd3c718980119d5ec358511eb2f14796c24d
Daisy Nkweteyim <nkweteyimdaisy@gmail.com>
2015-08-05 03:24:05 +0100

    refactored code

--- src/common/CommonCode.java
+++ src/common/CommonCode.java
@@ -1,18 +1,15 @@
 /**
- * This class is a subclass to other classes.  It will serve as the common code which these classes will share.
- * It describes what will happen before and after the actual tests.
+ * This class provides common functionalities for other classes.
  * 
- * @author Daisy Nkweteyim
+ * @author daisy
  */
 
 package common;
 
 import java.util.concurrent.TimeUnit;
-
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -30,8 +27,6 @@ public class CommonCode {
 		driver = new FirefoxDriver();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	
-	
-	//public void login() {
 		expectedTitle = "Welcome !";
 		driver.get(baseURL);
 		driver.manage().window().maximize();
@@ -39,42 +34,15 @@ public class CommonCode {
 		Assert.assertEquals(expectedTitle, actualTitle);
 		
 		WebElement username = driver.findElement(By.name("username"));
-		username.sendKeys("admin");
+		username.sendKeys(utilities.Constants.defaultUsername);
 		WebElement password = driver.findElement(By.name("password"));
-		password.sendKeys("mypassword");
-		
-		WebElement submit = driver.findElement(By.name("submit"));
+		password.sendKeys(utilities.Constants.defaultPassword);		
+		WebElement submit = driver.findElement(By.name(utilities.Constants.sumbitButton));
 		submit.click();
 		
-		//wait to load page to get title
+		//wait for load page to get title
 		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);		
 		Assert.assertEquals("Dashboard", driver.getTitle());
-	}
-	
-	
-	public void addpost(String title, String description) {
-		
-		expectedTitle = "Malaria : infoHub";
-			
-		driver.get(baseURL + "/malaria/" );
-		String actualTitle = driver.getTitle();
-		//Assert.assertEquals(expectedTitle, actualTitle);
-	
-		
-		//TODO: input arbitrary values (more generic)perhaps from excel sheet.
-		
-		// click on new post
-		driver.findElement(By.xpath("html/body/div[2]/div/button")).click();
-		driver.findElement(By.xpath
-				("html/body/div[2]/div[1]/form/div[1]/div[3]/input")).sendKeys(title);
-		driver.findElement(By.xpath
-				("html/body/div[2]/div[1]/form/div[2]/div[3]/textarea")).sendKeys
-				(description);		
-
-		//click on submit
-		driver.findElement(By.xpath("html/body/div[2]/div[1]/form/center/div/div[2]/input")).click();
-		String result = driver.findElement(By.xpath("html/body/div[2]/center[1]/h4")).getText();
-		System.out.println(result);
 	}
 	
 	@After
--- src/common/DataProviderCommonCode.java
+++ src/common/DataProviderCommonCode.java
@@ -1,8 +1,14 @@
+/**
+ * This class provides common functionalities for other classes.
+ */
+
 package common;
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -17,6 +23,12 @@ public class DataProviderCommonCode {
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.get(utilities.Constants.baseURL);
 		driver.manage().window().maximize();
+		WebElement username = driver.findElement(By.name("username"));
+		username.sendKeys(utilities.Constants.defaultUsername);
+		WebElement password = driver.findElement(By.name("password"));
+		password.sendKeys(utilities.Constants.defaultPassword);
+		WebElement submit = driver.findElement(By.name("submit"));
+		submit.click();
 	}
 	
 	@AfterMethod
