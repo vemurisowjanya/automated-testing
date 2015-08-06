91a2dfafc4873768b1d055ab2418375cd0e575f3
Daisy Nkweteyim <nkweteyimdaisy@gmail.com>
2015-08-06 19:56:23 +0100

    test all links on the homepage

--- src/home_page_features/Home_Page_Malaria.java
+++ src/home_page_features/Home_Page_Malaria.java
@@ -1,40 +1,37 @@
 /**
- * The dashboard test
+ * This class makes sure all the links in the home page are functional.
  * 
- * @author Daisy Nkweteyim
+ * @author daisy
  */
 
 package home_page_features;
 
-import org.junit.Assert;
+import java.util.List;
+
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import common.CommonCode;
 
-
-public class Home_Page_Malaria extends common.CommonCode {
-	
-	private String expectedTitle; 
+public class Home_Page_Malaria extends CommonCode {
 		
+	//tests all the links in the homepage
 	@Test
-	public void DashboardTest() {
-		expectedTitle = "Dashboard";
-
-		driver.get(baseURL);
-		driver.manage().window().maximize();
-		String actualTitle = driver.getTitle();
-		Assert.assertTrue("Expected value is not the same as the actual value.",
-				expectedTitle.equals(actualTitle));
+	public void DashboardTest() throws InterruptedException {
 		
-		//MACC NavBar print
-		driver.findElement(By.xpath("html/body/div[1]/div/div/div/div[2]/a/h1")).click();
+		List<WebElement> links = driver.findElements(By.tagName("a")); 
+		 String[] all_links = new String [links.size()];
+		for(int i = 0; i < links.size(); i++) {
+			all_links[i] = links.get(i).getAttribute("href");
+		}
 		
-		//MACC NavBar icon
-		driver.findElement(By.xpath("html/body/div[1]/div/div/div/div[1]/a/img")).click();
+		for(int i = 0; i < links.size(); i++){
+			driver.navigate().to(all_links[i]);
+			Thread.sleep(2000);
+		}
 		
-		//Malaria link
-		driver.findElement(By.xpath
-				("html/body/div[2]/div[1]/div/center/h2/a/font/u")).click(); //get both button and underlined
-			
+		
+	
 	}
 	
 }
