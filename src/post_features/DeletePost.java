749e2fd05e95c9432087e25d388804f77e5abaf7
Daisy Nkweteyim <nkweteyimdaisy@gmail.com>
2015-08-07 05:28:42 +0100

    added delete post tests

--- src/post_features/DeletePost.java
+++ src/post_features/DeletePost.java
@@ -1,27 +1,31 @@
 /**
  * This class tests the delete post functionality.
  * 
- * @author Daisy Nkweteyim
+ * @author daisy
  */
 
 package post_features;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+import common.CommonCode;
 
-public class DeletePost extends common.CommonCode {
-	private String expectedTitle;
+public class DeletePost extends CommonCode {
 	
 	@Test
 	public void deletepost() {
-		baseURL = "http://localhost:8001";
-		expectedTitle = "Malaria : infoHub";
 			
-		driver.get(baseURL + "/malaria/" );
-		driver.manage().window().maximize();
-		String actualTitle = driver.getTitle();
-		//Assert.assertEquals(expectedTitle, actualTitle);
-		
-		//TODO
+		driver.get(utilities.Constants.list_of_posts );
+		try {			
+			WebElement deleteButton = driver.findElement(By.linkText("Delete"));
+			deleteButton.click();
+			driver.findElement(By.xpath("html/body/div[1]/div/form/div[2]/div/button")).click();
+			}
+			catch(NoSuchElementException e) {
+				System.out.println("No such element: Delete");
+			}
 	}
 }
