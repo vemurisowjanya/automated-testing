99a448e346399d14e5724c844013d005ac4016a8
KeshaShah <kesha.shah1106@gmail.com>
2015-08-17 02:14:19 +0530

    Contact Page Constant Added

---
+++ UshahidiTests/src/main/java/ushahidi/commons/ContactsPageConstants.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package ushahidi.commons;
+
+/**
+ * @author Kesha
+ *
+ */
+public class ContactsPageConstants {
+	public static final String name_req = "The name field is required.";
+	public static final String email_req = "The Email field is required.";
+	public static final String sub_req = "The subject field is required.";
+	public static final String msg_req = "The message field is required.";
+	public static final String sec_req = "Please enter the security code.";
+	public static final String success = "Your Message Has Been Sent!";
+}
