1cb82328c7dfa00355a5d3031b0e126d67f8d1cf
Madi Vachon <maditparis@gmail.com>
2016-06-06 10:14:04 -0400

    Created a test suite for creating an account

---
+++ PLTTests/src/testSuites/AllCreateAccountTests.java
@@ -0,0 +1,15 @@
+package testSuites;
+import org.junit.*;
+import login.*;
+
+/*
+ * @author Madi Paris
+ * This test suite will run all tests in the package CreateAccount
+ */
+
+ @RunWith(Suite.class)
+ @SuiteClasses({CreateAccount.class})
+ public class AllCreateAccountPageTests
+ {
+
+ }
