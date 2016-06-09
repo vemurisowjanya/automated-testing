119d3649efeb42f1e717131e5a224e8d39b9013a
Anjali05 <shally.hp5@gmail.com>
2016-06-09 22:59:59 +0530

    Adding log4j files.

---
+++ PCSAWebTests/PCSAWebFramework/src/Logs/log4j.java
@@ -0,0 +1,13 @@
+/*
+ * @suthor Anjali
+ * @since 2016-06-08
+ * This class initialises a Logger object
+ */
+package Logs;
+import org.apache.log4j.Logger;
+
+
+public class log4j 
+{
+	public static Logger Log = Logger.getLogger(log4j.class.getName());
+}
