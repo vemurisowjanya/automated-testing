c719440b14b65364caaa5b64c9de27adfca25738
Anjali05 <shally.hp5@gmail.com>
2016-06-09 23:04:17 +0530

    Adding Recommended pagee package for framework and test.

---
+++ PCSAWebTests/PCSAWebTests/src/RecommendedPagesTests/safetyStrategiesHelplineTest.java
@@ -0,0 +1,23 @@
+/*
+ * @author Anjali
+ * @since 2016-06-09
+ * This script tests the navigation to the Safety Strategies Page on click of the button  on Recommended Pages.
+ */
+package RecommendedPagesTests;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import BaseTests.commonTest;
+import Logs.log4j;
+import RecommendedPagesFeatures.recommendedPage;
+
+public class safetyStrategiesHelplineTest extends commonTest
+{
+	@Test
+	public void test()
+	{
+		log4j.Log.info("Starting safetyStrategiesHelplineTest");
+		recommendedPage.gotoRecommendedPage();
+		Assert.assertEquals(true, recommendedPage.isAtSafetyStrategiesHelpline(), "Landing Page Navigation Failed");
+		log4j.Log.info("Test completed");
+	}
+}
