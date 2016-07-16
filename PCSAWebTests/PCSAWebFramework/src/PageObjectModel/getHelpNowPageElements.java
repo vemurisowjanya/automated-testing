66c5558c61d71c68dc2c9e7f2f8a1a34df78f89a
Anjali05 <shally.hp5@gmail.com>
2016-07-16 21:52:40 +0530

    adding more tests for get help now with minor updates in other files.

--- PCSAWebTests/PCSAWebFramework/src/PageObjectModel/getHelpNowPageElements.java
+++ PCSAWebTests/PCSAWebFramework/src/PageObjectModel/getHelpNowPageElements.java
@@ -14,6 +14,15 @@ public class getHelpNowPageElements
 	public static WebElement element;
 	
 	/*
+	 * @return Button to take to the second page of Get Help Now.
+	 */
+	public static WebElement toGetHelpPage2()
+	{
+		element = driver.Instance.findElement(constants.toGetHelpPage2);
+		return element;
+	}
+
+	/*
 	 * @return Get help page title on the Get Help Page.
 	 */
 	public static WebElement gethelpPageTitle()
@@ -68,7 +77,7 @@ public class getHelpNowPageElements
 	}
 	
 	/*
-	 * @return Voice Call Button for Contact PCMO
+	 * @return Voice Call Button for Contact PCMO.
 	 */
 	public static WebElement pcmoCallButton()
 	{
@@ -77,7 +86,7 @@ public class getHelpNowPageElements
 	}
 	
 	/*
-	 * @return Send Message Button for Contact PCMO
+	 * @return Send Message Button for Contact PCMO.
 	 */
 	public static WebElement pcmoSMSButton()
 	{
@@ -86,7 +95,7 @@ public class getHelpNowPageElements
 	}
 	
 	/*
-	 * @return Voice Call Button for Contact SSM
+	 * @return Voice Call Button for Contact SSM.
 	 */
 	public static WebElement ssmCallButton()
 	{
@@ -95,7 +104,7 @@ public class getHelpNowPageElements
 	}
 	
 	/*
-	 * @return Send Message Button for Contact SSM
+	 * @return Send Message Button for Contact SSM.
 	 */
 	public static WebElement ssmSMSButton()
 	{
@@ -104,7 +113,7 @@ public class getHelpNowPageElements
 	}
 	
 	/*
-	 * @return Voice Call Button for Contact SARL
+	 * @return Voice Call Button for Contact SARL.
 	 */
 	public static WebElement sarlCallButton()
 	{
@@ -113,7 +122,7 @@ public class getHelpNowPageElements
 	}
 	
 	/*
-	 * @return Send Message Button for Contact SARL
+	 * @return Send Message Button for Contact SARL.
 	 */
 	public static WebElement sarlSMSButton()
 	{
@@ -122,7 +131,7 @@ public class getHelpNowPageElements
 	}
 	
 	/*
-	 * @return Text Box for Send Message
+	 * @return Text Box for Send Message.
 	 */
 	public static WebElement smsBodyTextBox()
 	{
@@ -131,11 +140,138 @@ public class getHelpNowPageElements
 	}
 	
 	/*
-	 * @return Send SMS Button
+	 * @return Send SMS Button.
 	 */
 	public static WebElement sendSMSButton()
 	{
 		element = driver.Instance.findElement(constants.sendSMSButton);
 		return element;
 	}
+	
+	/*
+	 * @return Button for "PC Saves Anonymous Helpline" on the second page of Get Help Now.
+	 */
+	public static WebElement PCHelpline()
+	{
+		element = driver.Instance.findElement(constants.pcSavesAnonymousHelpline);
+		return element;
+	}
+	
+	/*
+	 * @return Button for "Office of Victim Advocacy" on the second page of Get Help Now.
+	 */
+	public static WebElement officeOfVictimAdvocacy()
+	{
+		element = driver.Instance.findElement(constants.officeOfVictimAdvocacy);
+		return element;
+	}
+	
+	/*
+	 * @return Button for "Office of Inspector General" on the second page of Get Help Now.
+	 */
+	public static WebElement officeOfInspectorGeneral()
+	{
+		element = driver.Instance.findElement(constants.officeOfInspectorGeneral);
+		return element;
+	}
+	
+	/*
+	 * @return Button for "Office of Civil Rights and Diversity" on the second page of Get Help Now.
+	 */
+	public static WebElement officeOfCivilRightsAndDiversity()
+	{
+		element = driver.Instance.findElement(constants.officeOfCivilRightsAndDiversity);
+		return element;
+	}
+	
+	/*
+	 * @return Button for "Contact Now" under PC Saves Anonymous Helpline.
+	 */
+	public static WebElement contactNowPCHelpline()
+	{
+		element = driver.Instance.findElement(constants.contactNowButtonPCSavesAnonymousHelpline);
+		return element;
+	}
+	
+	/*
+	 * @return Button for "Contact Now" under Office of Victim Advocacy.
+	 */
+	public static WebElement contactNowButtonOfficeOfVictimAdvocacy()
+	{
+		element = driver.Instance.findElement(constants.contactNowButtonOfficeOfVictimAdvocacy);
+		return element;
+	}
+	
+	/*
+	 * @return Button for "Contact Now" under Office of Inspector General.
+	 */
+	public static WebElement contactNowButtonOfficeOfInspectorGeneral()
+	{
+		element = driver.Instance.findElement(constants.contactNowButtonOfficeOfInspectorGeneral);
+		return element;
+	}
+	
+	/*
+	 * @return Button for "Contact Now" under Office of Civil Rights and Diversity.
+	 */
+	public static WebElement contactNowButtonoOfficeOfCivilRightsAndDiversity()
+	{
+		element = driver.Instance.findElement(constants.contactNowButtonoOfficeOfCivilRightsAndDiversity);
+		return element;
+	}
+	
+	/*
+	 * @return Button for "Voice Call" from the pop up that appears after Clicking on Contact Now.
+	 */
+	public static WebElement voiceCallButton()
+	{
+		element = driver.Instance.findElement(constants.voiceCallButton);
+		return element;
+	}
+	
+	/*
+	 * @return Button for "Send Message" from the pop up that appears after Clicking on Contact Now.
+	 */
+	public static WebElement sendMessageButton()
+	{
+		element = driver.Instance.findElement(constants.sendMessageButton);
+		return element;
+	}
+	
+	/*
+	 * @return Link to Learn More on PC Saves Anonymous Helpline.
+	 */
+	public static WebElement pcSavesLearnMoreLink()
+	{
+		element = driver.Instance.findElement(constants.pcSavesLearnMoreLink);
+		return element;
+	}
+	
+	/*
+	 * @return Link to Email on Office of Victim Advocacy.
+	 */
+	public static WebElement officeOfVictimAdvocacyEmailLink()
+	{
+		element = driver.Instance.findElement(constants.officeOfVictimAdvocacyEmailLink);
+		return element;
+	}
+	
+	/*
+	 * @return Link to Learn More on Office of Inspector General.
+	 */
+	public static WebElement officeOfInspectorGeneralLearnMoreLink()
+	{
+		element = driver.Instance.findElement(constants.officeOfInspectorGeneralLearnMoreLink);
+		return element;
+	}
+	
+	/*
+	 * @return Link to Email on Office of Civil Rights and Diversity.
+	 */
+	public static WebElement officeOfCivilRightsAndDiversityEmailLink()
+	{
+		element = driver.Instance.findElement(constants.officeOfCivilRightsAndDiversityEmailLink);
+		return element;
+	}
+	
 }
